17b5cd9479777d361cdd60dd0de45738f4c9c45a
SiarheiH <halavachsergey@gmail.com>
2018-04-25 20:54:46 +0300

    third 1

--- src/My.java
+++ src/My.java
@@ -3,5 +3,7 @@ public class My {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        System.out.println("I'am this branch");
+        System.out.println("I'am this branch");
     }
 }
